e9567c3ce0956317a85d3047fcdbe997a88f900b
703lovelost <dolphinhunter@inbox.ru>
2025-03-12 23:05:57 +0700

    Add contains tests

--- main.cpp
+++ main.cpp
@@ -16,6 +16,21 @@ TEST(CandleTest, BodyContainsLogic) {
     EXPECT_FALSE(candle.body_contains(130));
 }
 
+TEST(CandleTest, ContainsMaxValue) {
+    Candle candle(100, 150, 90, 120);
+    EXPECT_TRUE(candle.contains(150));
+}
+
+TEST(CandleTest, ContainsMinValue) {
+    Candle candle(100, 150, 90, 120);
+    EXPECT_TRUE(candle.contains(90));
+}
+
+TEST(CandleTest, ContainsLogic) {
+    Candle candle(100, 150, 90, 120);
+    EXPECT_FALSE(candle.contains(80));
+}
+
 int main(int argc, char **argv) {
     ::testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
